272652148aa046b4a24c0d032970c8ab10b677a7
UnLakhs <apostolos052@gmail.com>
2026-04-19 20:08:18 +0200

    added text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-
 const Home = () => {
   return (
-    <div className="flex flex-col items-center w-full">
+    <div className="flex flex-col items-center w-full gap-8">
       <h1 className="text-4xl font-bold ">Kyrgidis Apostolos</h1>
+      <p className="w-5xl text-xl text-foreground text-left font-light">
+        I am a Fullstack developer focused on building real-world web applications
+        using Next.js, TypeScript, and MongoDB. I have developed
+        production-ready projects, including a SaaS platform for automated SMS
+        notifications and customer management. I enjoy designing responsive,
+        user-friendly interfaces while building scalable backend systems.
+        Continuously improving my skills, I aim to deliver efficient and
+        practical software solutions.
+      </p>
     </div>
-  )
-}
+  );
+};
 export default Home;
