a96388e29f8d9c2c48efadf834d024c9640f3bd8
UnLakhs <apostolos052@gmail.com>
2026-04-20 16:26:53 +0300

    made the social links look a bit better

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import {FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { FiFileText } from "react-icons/fi";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center w-full gap-8">
       <h1 className="text-4xl font-bold ">Kyrgidis Apostolos</h1>
-      <p className="w-5xl text-xl text-foreground text-left font-light">
+      <p className="sm:w-5xl text-xl text-foreground text-left font-light">
         I am a Fullstack developer focused on building real-world web
         applications using Next.js, TypeScript, and MongoDB. I have developed
         production-ready projects, including a SaaS platform for automated SMS
@@ -16,22 +16,40 @@ const Home = () => {
         practical software solutions.
       </p>
       {/* socials */}
-      <div className="flex gap-4 items-center">
+      <div className="flex items-center gap-6">
         {/* CV */}
-        <Link href="/cv.pdf" target="_blank">
-          <FiFileText className="w-6 h-6 hover:opacity-70 transition" />
+        <Link
+          href="/cv.pdf"
+          target="_blank"
+          className="flex items-center gap-2 hover:opacity-70 transition"
+        >
+          <FiFileText className="w-5 h-5" />
+          <span>CV</span>
         </Link>
 
         {/* GitHub */}
-        <Link href="https://github.com/UnLakhs" target="_blank">
-          <FaGithub className="w-6 h-6 hover:opacity-70 transition" />
+        <Link
+          href="https://github.com/UnLakhs"
+          target="_blank"
+          className="flex items-center gap-2 hover:opacity-70 transition"
+        >
+          <FaGithub className="w-5 h-5" />
+          <span>GitHub</span>
         </Link>
 
         {/* LinkedIn */}
-        <Link href="https://www.linkedin.com/in/apostolos-kyrgidis/" target="_blank">
-          <FaLinkedin className="w-6 h-6 hover:opacity-70 transition" />
+        <Link
+          href="https://www.linkedin.com/in/apostolos-kyrgidis/"
+          target="_blank"
+          className="flex items-center gap-2 hover:opacity-70 transition"
+        >
+          <FaLinkedin className="w-5 h-5" />
+          <span>LinkedIn</span>
         </Link>
       </div>
+
+      {/* Next page */}
+      <a className="ml-auto hover:opacity-70 transition" href="./AboutMe">About Me →</a>
     </div>
   );
 };
