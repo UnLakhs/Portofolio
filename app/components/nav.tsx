2a4ea5e12d676af7f40cca56a5fce66c46c4b61b
UnLakhs <apostolos052@gmail.com>
2026-04-20 02:07:38 +0200

    better styling

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,14 +3,14 @@ const Nav = () => {
         <nav className="h-full py-6 pr-4 overflow-auto no-scrollbar lg:py-8 w-52">
             <div className="flex gap-1 flex-col ml-6">
                 <h4 className="font-semibold p-2 rounded-md text-lg">Sections</h4>
-                <div className="flex flex-col gap-1">
-                    <span className="hover:bg-gray-300 p-2 rounded-md text-lg transition duration-200">Introduction</span>
-                    <span className="hover:bg-gray-300 p-2 rounded-md text-lg transition duration-200">About Me</span>
-                    <span className="hover:bg-gray-300 p-2 rounded-md text-lg transition duration-200">Projects</span>
-                    <span className="hover:bg-gray-300 p-2 rounded-md text-lg transition duration-200">Skills & tools</span>
-                    <span className="hover:bg-gray-300 p-2 rounded-md text-lg transition duration-200">Experience</span>
-                    <span className="hover:bg-gray-300 p-2 rounded-md text-lg transition duration-200">Education</span>
-                    <span className="hover:bg-gray-300 p-2 rounded-md text-lg transition duration-200">Contact</span>
+                <div className="flex flex-col text-lg gap-1">
+                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Introduction</span>
+                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">About Me</span>
+                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Projects</span>
+                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Skills & tools</span>
+                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Experience</span>
+                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Education</span>
+                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Contact</span>
                 </div>
 
             </div>
